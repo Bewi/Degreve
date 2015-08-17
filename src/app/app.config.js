8e7e5a34347320b787edb620f64547db0313679b
Bewi <benjamin.wilfart@live.be>
2015-08-17 20:51:58 +0200

    Angular-hotkeys configuration
    Configure hotkeys help menu title
    Fix issue on layout for the same view

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -5,13 +5,16 @@
       .module('app')
       .config(configBlock);
 
-  /*@ngInject*/
-  function configBlock (NotificationProvider, blockUIConfig, ElectronProvider) {
+  /* @ngInject */
+  function configBlock (NotificationProvider, blockUIConfig, hotkeysProvider, ElectronProvider) {
     NotificationProvider.setOptions({
       startTop: 55
     });
 
     blockUIConfig.template = '<div class="block-ui-overlay"></div><div class="box-loading"></div>';
+
+    hotkeysProvider.templateTitle = 'Raccourcis clavier';
+    hotkeysProvider.cheatSheetDescription = "Affiche / masque ce menu d'aide";
 
     ElectronProvider.activate();
   }
