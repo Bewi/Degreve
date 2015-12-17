6b3a5d3b21f61ad40431b67853f4592a1cff8496
Bewi <benjamin.wilfart@live.be>
2015-12-17 08:30:52 +0100

    improve table styling

--- src/app/components/dw-order-direction.directive.js
+++ src/app/components/dw-order-direction.directive.js
@@ -8,7 +8,7 @@
     function dwOrderDirection() {
         var directive = {
             restrict: 'EA',
-            template: '<i class="fa fa-chevron-up pull-right" ng-class="{true: \'fa-chevron-up\', false: \'fa-chevron-down\' }[dm.query.orderByDirection == 1]" ng-if="dm.query.orderBy == dm.orderBy"></i>',
+            template: '<i class="fa pull-right" ng-class="{true: \'fa-sort-asc\', false: \'fa-sort-desc\' }[dm.query.orderByDirection == 1]" ng-if="dm.query.orderBy == dm.orderBy"></i>',
             scope: {
               orderBy: "@",
               query: "=orderDirectionQuery"
