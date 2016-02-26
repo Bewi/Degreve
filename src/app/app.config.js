f144966cb59e2ed9485b0cf2a7edd68ed241e14a
bewi <benjamin.wilfart@gmail.com>
2016-02-26 20:31:55 +0100

    Moved app.routes logic to app.config && remove file

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,27 +1,29 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-      .module('app')
-      .config(configBlock);
+    angular
+        .module('app')
+        .config(configBlock);
 
-  /* @ngInject */
-  function configBlock (NotificationProvider, blockUIConfig, hotkeysProvider, ElectronProvider) {
-    NotificationProvider.setOptions({
-      startTop: 55
-    });
-
-    blockUIConfig.template = '<div class="block-ui-overlay"></div><div class="box-loading"></div>';
-    blockUIConfig.requestFilter = function(config) {
-        if (!config.params || config.params.blockUi === undefined)
-            return true;
+    /* @ngInject */
+    function configBlock ($urlRouterProvider, NotificationProvider, blockUIConfig, hotkeysProvider, ElectronProvider) {
+        $urlRouterProvider.otherwise("/invoices");
         
-        return config.params.blockUi;
-    };
+        NotificationProvider.setOptions({
+        startTop: 55
+        });
 
-    hotkeysProvider.templateTitle = 'Raccourcis clavier';
-    hotkeysProvider.cheatSheetDescription = "Affiche / masque ce menu d'aide";
+        blockUIConfig.template = '<div class="block-ui-overlay"></div><div class="box-loading"></div>';
+        blockUIConfig.requestFilter = function(config) {
+            if (!config.params || config.params.blockUi === undefined)
+                return true;
+            
+            return config.params.blockUi;
+        };
 
-    ElectronProvider.activate();
-  }
+        hotkeysProvider.templateTitle = 'Raccourcis clavier';
+        hotkeysProvider.cheatSheetDescription = "Affiche / masque ce menu d'aide";
+
+        ElectronProvider.activate();
+    }
 }());
